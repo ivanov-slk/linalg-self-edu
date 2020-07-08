3f63b229bbaf7fb9f6da1e461169b0e41f20df1a
Slav <ivanov.slk@abv.bg>
2020-07-08 21:58:23 +0300

    implemented diagonalmatrix factory function

--- test/test_matrix.hpp
+++ test/test_matrix.hpp
@@ -2,6 +2,7 @@
 
 #include "matrix_tests/test_matrix_arithmetic.hpp"
 #include "matrix_tests/test_matrix_correctness.hpp"
+#include "matrix_tests/test_matrix_factory.hpp"
 #include "matrix_tests/test_matrix_multiplication.hpp"
 #include "matrix_tests/test_matrix_properties.hpp"
 #include "matrix_tests/test_matrix_shape_utilities.hpp"
