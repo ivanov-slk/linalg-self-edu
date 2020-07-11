104740c2f2af73ea3840e12a5262fa65ce353831
Slav <ivanov.slk@abv.bg>
2020-07-11 10:17:16 +0300

    implemented flattening from a matrix of matrices

--- src/matrix/matrix_factory.hpp
+++ src/matrix/matrix_factory.hpp
@@ -7,6 +7,7 @@
  */
 template <Number T>
 class MakeDiagonal
+// Most of the code here is duplicated. Consider refactoring to lambda or something.
 {
 public:
     /**
@@ -65,6 +66,68 @@ public:
                 }
             }
             out.push_back(temp_row);
+        }
+        return Matrix<T>{out};
+    }
+};
+
+/**
+ * @brief Makes a matrix with all elements equal to a given value.
+ */
+template <Number T>
+class MakeSameElement
+{
+public:
+    Matrix<T> operator()(T value, int n_rows, int n_cols)
+    {
+        std::vector<std::vector<T>> out;
+        for (int i = 0; i < n_rows; ++i)
+        {
+            std::vector<T> temp_row;
+            for (int j = 0; j < n_cols; ++j)
+            {
+                temp_row.push_back(value);
+            }
+            out.push_back(temp_row);
+        }
+        return Matrix<T>{out};
+    }
+};
+
+/**
+ * @brief Makes a matrix from a vector of matrices.
+ * 
+ * This is implemented for the QR decomposition, so consider generalizing/refactoring it.
+ * And consider making the implementation more efficient, since now is sort of terrible.
+ */
+template <Number T>
+class FlattenVectorOfMatrices
+{
+public:
+    Matrix<T> operator()(const std::vector<std::vector<Matrix<T>>> &matrices)
+    {
+        // Currently, the checks that the inputs can actually produce a valid matrix are delegated
+        // to the Matrix constructor at the end of the method. This is fine for small matrices, but
+        // for large matrices it would be better to throw at the beginning of the method and avoid
+        // all the loops.
+
+        std::vector<std::vector<T>> out;
+        for (auto &row_of_matrices : matrices)
+        {
+            int n_rows = row_of_matrices.at(0).get_shape().first;
+            for (int i = 0; i < n_rows; ++i)
+            {
+                std::vector<T> temp_row;
+                for (auto &matrix : row_of_matrices)
+                {
+                    int n_cols = matrix.get_shape().second;
+                    for (int j = 0; j < n_cols; ++j)
+                    {
+                        temp_row.push_back(matrix.extract_element(i, j));
+                    }
+                }
+                out.push_back(temp_row);
+            }
         }
         return Matrix<T>{out};
     }
--- test/matrix_tests/test_matrix_factory.hpp
+++ test/matrix_tests/test_matrix_factory.hpp
@@ -63,3 +63,64 @@ TEST(MatrixFactoryTests, MakeDiagonalEmpty)
     Matrix<float> correct;
     ASSERT_EQ(testable, correct);
 }
+
+TEST(MatrixFactoryTests, MakeSameElement)
+{
+    Matrix<float> testable = MakeSameElement<float>()(1.15, 2, 3);
+    Matrix<float> correct{
+        std::vector<std::vector<float>>{
+            {1.15, 1.15, 1.15},
+            {1.15, 1.15, 1.15}}};
+    ASSERT_EQ(testable, correct);
+}
+
+TEST(MatrixFactoryTests, MakeSameElementEmpty)
+{
+    Matrix<float> test_input;
+    Matrix<float> testable = MakeDiagonal<float>()(test_input);
+    Matrix<float> correct;
+    ASSERT_EQ(testable, correct);
+}
+
+TEST(MatrixFactoryTests, FlattenVectorOfMatrices)
+{
+    Matrix<float> mat1 = MakeSameElement<float>()(1.11, 3, 2);
+    Matrix<float> mat2 = MakeSameElement<float>()(2.22, 3, 5);
+    Matrix<float> mat3 = MakeSameElement<float>()(3.33, 2, 3);
+    Matrix<float> mat4 = MakeSameElement<float>()(4.44, 2, 4);
+    std::vector<std::vector<Matrix<float>>> input_vector{
+        {mat1, mat2},
+        {mat3, mat4}};
+    Matrix<float> testable = FlattenVectorOfMatrices<float>()(input_vector);
+    Matrix<float> correct{
+        std::vector<std::vector<float>>{
+            {1.11, 1.11, 2.22, 2.22, 2.22, 2.22, 2.22},
+            {1.11, 1.11, 2.22, 2.22, 2.22, 2.22, 2.22},
+            {1.11, 1.11, 2.22, 2.22, 2.22, 2.22, 2.22},
+            {3.33, 3.33, 3.33, 4.44, 4.44, 4.44, 4.44},
+            {3.33, 3.33, 3.33, 4.44, 4.44, 4.44, 4.44}}};
+    compare_two_matrices(testable, correct);
+}
+
+TEST(MatrixFactoryTests, FlattenVectorOfMatricesEmpty)
+{
+    Matrix<float> mat1, mat2, mat3, mat4;
+    std::vector<std::vector<Matrix<float>>> input_vector{
+        {mat1, mat2},
+        {mat3, mat4}};
+    Matrix<float> testable = FlattenVectorOfMatrices<float>()(input_vector);
+    Matrix<float> correct;
+    compare_two_matrices(testable, correct);
+}
+
+TEST(MatrixFactoryTests, FlattenVectorOfMatricesThrows)
+{
+    Matrix<float> mat1 = MakeSameElement<float>()(1.11, 3, 2);
+    Matrix<float> mat2 = MakeSameElement<float>()(2.22, 3, 5);
+    Matrix<float> mat3 = MakeSameElement<float>()(3.33, 2, 3);
+    Matrix<float> mat4 = MakeSameElement<float>()(4.44, 2, 3); // should be 4 to be correct
+    std::vector<std::vector<Matrix<float>>> input_vector{
+        {mat1, mat2},
+        {mat3, mat4}};
+    ASSERT_THROW(FlattenVectorOfMatrices<float>()(input_vector), BadDimensionsException);
+}
