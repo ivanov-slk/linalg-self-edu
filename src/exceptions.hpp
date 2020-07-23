f320620b0480054fcf3dfc2715e67120c2931bdf
Slav <ivanov.slk@abv.bg>
2020-07-23 11:06:41 +0300

    added new exception and a bit of refactoring

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -2,15 +2,25 @@
 #include <exception>
 #include <string>
 
-class BadDimensionsException : public std::exception
+class CustomException : public std::exception
 {
 private:
     std::string m_message;
 
 public:
-    BadDimensionsException(std::string msg) : m_message(msg){};
+    CustomException(std::string msg) : m_message(msg){};
     const char *what() const throw()
     {
         return m_message.c_str();
     }
+};
+
+class BadDimensionsException : public CustomException
+{
+    using CustomException::CustomException;
+};
+
+class BadMatrixPropertiesException : public CustomException
+{
+    using CustomException::CustomException;
 };
