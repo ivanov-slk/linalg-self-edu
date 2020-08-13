0b2bf416a403891d15ef0c38e9ab0e0c0ed211c5
Slav <ivanov.slk@abv.bg>
2020-08-13 13:57:12 +0300

    added additional check in SVD tests

--- test/matrix_tests/test_svd_decomposition.hpp
+++ test/matrix_tests/test_svd_decomposition.hpp
@@ -6,15 +6,16 @@ TEST(SVDDecompositionTests, SVDDecompositionSquare)
     Matrix<double> input_matrix{
         std::vector<std::vector<double>>{
             {4., 1., 3.},
-            { 8., 7., -2. },
-        { 1., 5., 4. }} };
+            {8., 7., -2.},
+            {1., 5., 4.}}};
     SVDDecomposer<double> testable;
     testable(input_matrix);
+    ASSERT_TRUE(testable.matrix_s.is_diagonal());
     Matrix<double> result = testable.matrix_u.multiply(testable.matrix_s)
-        .multiply(testable.matrix_v);
+                                .multiply(testable.matrix_v);
     compare_two_matrices(input_matrix, result);
     ASSERT_FLOAT_EQ(result.sum(-1, 1).extract_element(0, 0),
-        input_matrix.sum(-1, 1).extract_element(0, 0));
+                    input_matrix.sum(-1, 1).extract_element(0, 0));
 }
 
 TEST(SVDDecompositionTests, SVDDecompositionRows)
@@ -22,16 +23,17 @@ TEST(SVDDecompositionTests, SVDDecompositionRows)
     Matrix<double> input_matrix{
         std::vector<std::vector<double>>{
             {12., -51., 4.},
-            { 6., 167., -68. },
-            { -4., 24., -41. },
-        { 10., -24., 8. }} };
+            {6., 167., -68.},
+            {-4., 24., -41.},
+            {10., -24., 8.}}};
     SVDDecomposer<double> testable;
     testable(input_matrix);
+    ASSERT_TRUE(testable.matrix_s.is_diagonal());
     Matrix<double> result = testable.matrix_u.multiply(testable.matrix_s)
-        .multiply(testable.matrix_v);
+                                .multiply(testable.matrix_v);
     compare_two_matrices(input_matrix, result);
     ASSERT_FLOAT_EQ(result.sum(-1, 1).extract_element(0, 0),
-        input_matrix.sum(-1, 1).extract_element(0, 0));
+                    input_matrix.sum(-1, 1).extract_element(0, 0));
 }
 
 TEST(SVDDecompositionTests, SVDDecompositionColumns)
@@ -39,13 +41,14 @@ TEST(SVDDecompositionTests, SVDDecompositionColumns)
     Matrix<double> input_matrix{
         std::vector<std::vector<double>>{
             {12., -51., 4., 6.},
-            { 167., -68., -4., 24. },
-        { -41., 10., -24., 8. }} };
+            {167., -68., -4., 24.},
+            {-41., 10., -24., 8.}}};
     SVDDecomposer<double> testable;
     testable(input_matrix);
+    ASSERT_TRUE(testable.matrix_s.is_diagonal());
     Matrix<double> result = testable.matrix_u.multiply(testable.matrix_s)
-        .multiply(testable.matrix_v);
+                                .multiply(testable.matrix_v);
     compare_two_matrices(input_matrix, result);
     ASSERT_FLOAT_EQ(result.sum(-1, 1).extract_element(0, 0),
-        input_matrix.sum(-1, 1).extract_element(0, 0));
+                    input_matrix.sum(-1, 1).extract_element(0, 0));
 }
