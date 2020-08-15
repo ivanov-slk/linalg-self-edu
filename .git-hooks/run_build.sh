c13abf723f2bc805c488686cea9938d564cbea96
Slav <ivanov.slk@abv.bg>
2020-08-15 16:34:45 +0300

    build duration calculated in run_build instead of pre-commit hook

--- .git-hooks/run_build.sh
+++ .git-hooks/run_build.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
+start_time=$(date +%s)
 echo "$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Starting build..."
 set -e
 
@@ -12,4 +13,5 @@ cmake -D CMAKE_C_COMPILER=/usr/bin/gcc-10 -D CMAKE_CXX_COMPILER=/usr/bin/g++-10 
 make -j 3
 echo -e "\n$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Running tests..."
 test/run_tests
-echo -e "\n$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Build finished."
+end_time=$(date +%s)
+echo -e "\n$(printf '%(%Y-%m-%d %H:%M:%S)T\n' -1) [BUILD] Build finished for $(($end_time-$start_time)) seconds."
