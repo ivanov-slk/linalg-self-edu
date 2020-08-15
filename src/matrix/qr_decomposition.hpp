ade8e17194bb008dcf8969f209cad96cd4788328
Slav <ivanov.slk@abv.bg>
2020-08-15 21:35:11 +0300

    Mozilla style looks kind of better

--- src/matrix/qr_decomposition.hpp
+++ src/matrix/qr_decomposition.hpp
@@ -11,10 +11,11 @@
  * Note: the implementation follows
  * https://en.wikipedia.org/wiki/QR_decomposition#Using_Householder_reflections.
  */
-template <Number T> class QRDecomposer
+template<Number T>
+class QRDecomposer
 {
   private:
-    Matrix<T> create_householder_matrix(const Matrix<T> &vector, const Matrix<T> &basis, int k)
+    Matrix<T> create_householder_matrix(const Matrix<T>& vector, const Matrix<T>& basis, int k)
     {
         // calculate alpha = sign(x[k])||x||
         // the first element of `vector` is the k-th of the decomposable matrix
@@ -30,17 +31,18 @@ template <Number T> class QRDecomposer
 
         // calculate the Householder matrix
         int householder_rows = vector.get_shape().first;
-        Matrix<T> basis_matrix = MakeDiagonal<T>()(T(1.), householder_rows,
+        Matrix<T> basis_matrix = MakeDiagonal<T>()(T(1.),
+                                                   householder_rows,
                                                    householder_rows); // square
         Matrix<T> householder_matrix =
-            basis_matrix.subtract(unit_vector.multiply(unit_vector.transpose()).el_multiply(2.));
+          basis_matrix.subtract(unit_vector.multiply(unit_vector.transpose()).el_multiply(2.));
         return householder_matrix;
     }
 
   public:
     Matrix<T> matrix_q;
     Matrix<T> matrix_r;
-    void operator()(const Matrix<T> &matrix)
+    void operator()(const Matrix<T>& matrix)
     {
         // check dimensions and number of iterations
         int n_rows = matrix.get_shape().first;
@@ -52,25 +54,25 @@ template <Number T> class QRDecomposer
         Matrix<T> q_i_multiply_input = matrix; // copy (!!!), initialize
 
         // loop over the above number
-        for (int i = 0; i < n_iter; ++i)
-        {
+        for (int i = 0; i < n_iter; ++i) {
             // extract the two input vectors
             // calculate a Householder matrix
             Matrix<T> q_i = create_householder_matrix(
-                q_i_multiply_input
-                    .extract_submatrix(i, i, std::greater_equal<T>(), std::greater_equal<T>())
-                    .extract_column(0),
-                identity.extract_submatrix(i, i, std::greater_equal<T>(), std::greater_equal<T>())
-                    .extract_column(0),
-                i);
+              q_i_multiply_input
+                .extract_submatrix(i, i, std::greater_equal<T>(), std::greater_equal<T>())
+                .extract_column(0),
+              identity.extract_submatrix(i, i, std::greater_equal<T>(), std::greater_equal<T>())
+                .extract_column(0),
+              i);
 
             // resize the Householder matrix to the input matrix's
             // dimensions
             Matrix<T> upper_identity = MakeDiagonal<T>()(T(1), i, i);
             Matrix<T> upper_zeroes = MakeSameElement<T>()(T(0), i, n_rows - i);
             Matrix<T> lower_zeroes = MakeSameElement<T>()(T(0), n_rows - i, i);
-            std::vector<std::vector<Matrix<T>>> resize_placeholder{{upper_identity, upper_zeroes},
-                                                                   {lower_zeroes, q_i}};
+            std::vector<std::vector<Matrix<T>>> resize_placeholder{
+                { upper_identity, upper_zeroes }, { lower_zeroes, q_i }
+            };
             Matrix<T> q_i_resized = FlattenVectorOfMatrices<T>()(resize_placeholder);
 
             // store the Householder matrix
@@ -78,16 +80,14 @@ template <Number T> class QRDecomposer
 
             //// convert the almost zero elements to zero (they should be
             /// zeroes by theory anyways)
-            for (int j = i + 1; j <= n_iter; ++j)
-            {
+            for (int j = i + 1; j <= n_iter; ++j) {
                 q_i_multiply_input.set_element(T(0), j, i);
             }
             matrices_q.push_back(q_i_resized);
         }
         // use all stored Householder matrices to calculate Q and R
         matrix_r = q_i_multiply_input;
-        for (auto &matrix : matrices_q)
-        {
+        for (auto& matrix : matrices_q) {
             matrix_q = matrix.multiply(matrix_q);
         }
         matrix_q = matrix_q.transpose(); // expensive
